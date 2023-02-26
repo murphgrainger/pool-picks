0943241602eb2e8e6a38328fda9620f540c3d32e
Murph Grainger <murphgrainger@Murphs-Macbook.local>
2023-02-26 15:56:25 -0700

    changed athlete seeding to seed by tournament

--- data/masters.ts
+++ data/masters.ts
@@ -1,7 +1,722 @@
 export const mastersAthletes = [
     {
-        full_name: "Chris Kirk",
-        first_name: "Chris",
-        last_name: "Kirk"
+      "first_name": "Chris",
+      "last_name": "Kirk",
+      "full_name": "Chris Kirk"
+    },
+    {
+      "first_name": "Eric",
+      "last_name": "Cole",
+      "full_name": "Eric Cole"
+    },
+    {
+      "first_name": "Tyler",
+      "last_name": "Duncan",
+      "full_name": "Tyler Duncan"
+    },
+    {
+      "first_name": "Ryan",
+      "last_name": "Gerard",
+      "full_name": "Ryan Gerard"
+    },
+    {
+      "first_name": "Ben",
+      "last_name": "Martin",
+      "full_name": "Ben Martin"
+    },
+    {
+      "first_name": "Shane",
+      "last_name": "Lowry",
+      "full_name": "Shane Lowry"
+    },
+    {
+      "first_name": "Justin",
+      "last_name": "Suh",
+      "full_name": "Justin Suh"
+    },
+    {
+      "first_name": "Dylan",
+      "last_name": "Wu",
+      "full_name": "Dylan Wu"
+    },
+    {
+      "first_name": "David",
+      "last_name": "Lingmerth",
+      "full_name": "David Lingmerth"
+    },
+    {
+      "first_name": "Sepp",
+      "last_name": "Straka",
+      "full_name": "Sepp Straka"
+    },
+    {
+      "first_name": "Ben",
+      "last_name": "Taylor",
+      "full_name": "Ben Taylor"
+    },
+    {
+      "first_name": "Zach",
+      "last_name": "Johnson",
+      "full_name": "Zach Johnson"
+    },
+    {
+      "first_name": "Cameron",
+      "last_name": "Percy",
+      "full_name": "Cameron Percy"
+    },
+    {
+      "first_name": "Brandon",
+      "last_name": "Wu",
+      "full_name": "Brandon Wu"
+    },
+    {
+      "first_name": "Jim",
+      "last_name": "Herman",
+      "full_name": "Jim Herman"
+    },
+    {
+      "first_name": "Lee",
+      "last_name": "Hodges",
+      "full_name": "Lee Hodges"
+    },
+    {
+      "first_name": "Stephan",
+      "last_name": "Jaeger",
+      "full_name": "Stephan Jaeger"
+    },
+    {
+      "first_name": "Adrian",
+      "last_name": "Meronk",
+      "full_name": "Adrian Meronk"
+    },
+    {
+      "first_name": "Kramer",
+      "last_name": "Hickok",
+      "full_name": "Kramer Hickok"
+    },
+    {
+      "first_name": "Ryan",
+      "last_name": "Brehm",
+      "full_name": "Ryan Brehm"
+    },
+    {
+      "first_name": "Byeong",
+      "last_name": "Hun An",
+      "full_name": "Byeong Hun An"
+    },
+    {
+      "first_name": "Jhonattan",
+      "last_name": "Vegas",
+      "full_name": "Jhonattan Vegas"
+    },
+    {
+      "first_name": "MJ",
+      "last_name": "Daffue",
+      "full_name": "MJ Daffue"
+    },
+    {
+      "first_name": "Ben",
+      "last_name": "Griffin",
+      "full_name": "Ben Griffin"
+    },
+    {
+      "first_name": "Robby",
+      "last_name": "Shelton",
+      "full_name": "Robby Shelton"
+    },
+    {
+      "first_name": "Min",
+      "last_name": "Woo Lee",
+      "full_name": "Min Woo Lee"
+    },
+    {
+      "first_name": "Scott",
+      "last_name": "Harrington",
+      "full_name": "Scott Harrington"
+    },
+    {
+      "first_name": "Harrison",
+      "last_name": "Endycott",
+      "full_name": "Harrison Endycott"
+    },
+    {
+      "first_name": "Aaron",
+      "last_name": "Wise",
+      "full_name": "Aaron Wise"
+    },
+    {
+      "first_name": "Kevin",
+      "last_name": "Chappell",
+      "full_name": "Kevin Chappell"
+    },
+    {
+      "first_name": "Carson",
+      "last_name": "Young",
+      "full_name": "Carson Young"
+    },
+    {
+      "first_name": "Dylan",
+      "last_name": "Frittelli",
+      "full_name": "Dylan Frittelli"
+    },
+    {
+      "first_name": "Brett",
+      "last_name": "Drewitt",
+      "full_name": "Brett Drewitt"
+    },
+    {
+      "first_name": "Danny",
+      "last_name": "Willett",
+      "full_name": "Danny Willett"
+    },
+    {
+      "first_name": "Garrick",
+      "last_name": "Higgo",
+      "full_name": "Garrick Higgo"
+    },
+    {
+      "first_name": "Andrew",
+      "last_name": "Novak",
+      "full_name": "Andrew Novak"
+    },
+    {
+      "first_name": "William",
+      "last_name": "McGirt",
+      "full_name": "William McGirt"
+    },
+    {
+      "first_name": "Kevin",
+      "last_name": "Roy",
+      "full_name": "Kevin Roy"
+    },
+    {
+      "first_name": "Davis",
+      "last_name": "Riley",
+      "full_name": "Davis Riley"
+    },
+    {
+      "first_name": "Chesson",
+      "last_name": "Hadley",
+      "full_name": "Chesson Hadley"
+    },
+    {
+      "first_name": "Matt",
+      "last_name": "Wallace",
+      "full_name": "Matt Wallace"
+    },
+    {
+      "first_name": "Mark",
+      "last_name": "Hubbard",
+      "full_name": "Mark Hubbard"
+    },
+    {
+      "first_name": "Will",
+      "last_name": "Gordon",
+      "full_name": "Will Gordon"
+    },
+    {
+      "first_name": "Sungjae",
+      "last_name": "Im",
+      "full_name": "Sungjae Im"
+    },
+    {
+      "first_name": "Taylor",
+      "last_name": "Pendrith",
+      "full_name": "Taylor Pendrith"
+    },
+    {
+      "first_name": "Adam",
+      "last_name": "Schenk",
+      "full_name": "Adam Schenk"
+    },
+    {
+      "first_name": "Billy",
+      "last_name": "Horschel",
+      "full_name": "Billy Horschel"
+    },
+    {
+      "first_name": "Christiaan",
+      "last_name": "Bezuidenhout",
+      "full_name": "Christiaan Bezuidenhout"
+    },
+    {
+      "first_name": "Akshay",
+      "last_name": "Bhatia",
+      "full_name": "Akshay Bhatia"
+    },
+    {
+      "first_name": "Zac",
+      "last_name": "Blair",
+      "full_name": "Zac Blair"
+    },
+    {
+      "first_name": "Jimmy",
+      "last_name": "Walker",
+      "full_name": "Jimmy Walker"
+    },
+    {
+      "first_name": "Erik",
+      "last_name": "Barnes",
+      "full_name": "Erik Barnes"
+    },
+    {
+      "first_name": "Trevor",
+      "last_name": "Werbylo",
+      "full_name": "Trevor Werbylo"
+    },
+    {
+      "first_name": "Adam",
+      "last_name": "Svensson",
+      "full_name": "Adam Svensson"
+    },
+    {
+      "first_name": "Joseph",
+      "last_name": "Bramlett",
+      "full_name": "Joseph Bramlett"
+    },
+    {
+      "first_name": "Brandon",
+      "last_name": "Matthews",
+      "full_name": "Brandon Matthews"
+    },
+    {
+      "first_name": "Tano",
+      "last_name": "Goya",
+      "full_name": "Tano Goya"
+    },
+    {
+      "first_name": "Kelly",
+      "last_name": "Kraft",
+      "full_name": "Kelly Kraft"
+    },
+    {
+      "first_name": "Brice",
+      "last_name": "Garnett",
+      "full_name": "Brice Garnett"
+    },
+    {
+      "first_name": "Kyle",
+      "last_name": "Stanley",
+      "full_name": "Kyle Stanley"
+    },
+    {
+      "first_name": "Padraig",
+      "last_name": "Harrington",
+      "full_name": "Padraig Harrington"
+    },
+    {
+      "first_name": "Matthias",
+      "last_name": "Schwab",
+      "full_name": "Matthias Schwab"
+    },
+    {
+      "first_name": "Anders",
+      "last_name": "Albertson",
+      "full_name": "Anders Albertson"
+    },
+    {
+      "first_name": "Pierceson",
+      "last_name": "Coody",
+      "full_name": "Pierceson Coody"
+    },
+    {
+      "first_name": "Kevin",
+      "last_name": "Tway",
+      "full_name": "Kevin Tway"
+    },
+    {
+      "first_name": "Vincent",
+      "last_name": "Norrman",
+      "full_name": "Vincent Norrman"
+    },
+    {
+      "first_name": "Seonghyeon",
+      "last_name": "Kim",
+      "full_name": "Seonghyeon Kim"
+    },
+    {
+      "first_name": "Trace",
+      "last_name": "Crowe",
+      "full_name": "Trace Crowe"
+    },
+    {
+      "first_name": "Ryan",
+      "last_name": "Armour",
+      "full_name": "Ryan Armour"
+    },
+    {
+      "first_name": "J.T.",
+      "last_name": "Poston",
+      "full_name": "J.T. Poston"
+    },
+    {
+      "first_name": "J.B.",
+      "last_name": "Holmes",
+      "full_name": "J.B. Holmes"
+    },
+    {
+      "first_name": "Bill",
+      "last_name": "Haas",
+      "full_name": "Bill Haas"
+    },
+    {
+      "first_name": "Augusto",
+      "last_name": "Núñez",
+      "full_name": "Augusto Núñez"
+    },
+    {
+      "first_name": "Jason",
+      "last_name": "Dufner",
+      "full_name": "Jason Dufner"
+    },
+    {
+      "first_name": "Trevor",
+      "last_name": "Cone",
+      "full_name": "Trevor Cone"
+    },
+    {
+      "first_name": "Harry",
+      "last_name": "Hall",
+      "full_name": "Harry Hall"
+    },
+    {
+      "first_name": "Webb",
+      "last_name": "Simpson",
+      "full_name": "Webb Simpson"
+    },
+    {
+      "first_name": "Tyson",
+      "last_name": "Alexander",
+      "full_name": "Tyson Alexander"
+    },
+    {
+      "first_name": "Cody",
+      "last_name": "Gribble",
+      "full_name": "Cody Gribble"
+    },
+    {
+      "first_name": "Geoff",
+      "last_name": "Ogilvy",
+      "full_name": "Geoff Ogilvy"
+    },
+    {
+      "first_name": "Fabián",
+      "last_name": "Gómez",
+      "full_name": "Fabián Gómez"
+    },
+    {
+      "first_name": "Chad",
+      "last_name": "Ramey",
+      "full_name": "Chad Ramey"
+    },
+    {
+      "first_name": "Charley",
+      "last_name": "Hoffman",
+      "full_name": "Charley Hoffman"
+    },
+    {
+      "first_name": "Carl",
+      "last_name": "Yuan",
+      "full_name": "Carl Yuan"
+    },
+    {
+      "first_name": "Hank",
+      "last_name": "Lebioda",
+      "full_name": "Hank Lebioda"
+    },
+    {
+      "first_name": "Peter",
+      "last_name": "Malnati",
+      "full_name": "Peter Malnati"
+    },
+    {
+      "first_name": "Cam",
+      "last_name": "Davis",
+      "full_name": "Cam Davis"
+    },
+    {
+      "first_name": "Aaron",
+      "last_name": "Baddeley",
+      "full_name": "Aaron Baddeley"
+    },
+    {
+      "first_name": "Ben",
+      "last_name": "Silverman",
+      "full_name": "Ben Silverman"
+    },
+    {
+      "first_name": "Jonathan",
+      "last_name": "Byrd",
+      "full_name": "Jonathan Byrd"
+    },
+    {
+      "first_name": "Ben",
+      "last_name": "Crane",
+      "full_name": "Ben Crane"
+    },
+    {
+      "first_name": "Martin",
+      "last_name": "Trainer",
+      "full_name": "Martin Trainer"
+    },
+    {
+      "first_name": "Greyson",
+      "last_name": "Sigg",
+      "full_name": "Greyson Sigg"
+    },
+    {
+      "first_name": "Michael",
+      "last_name": "Gligic",
+      "full_name": "Michael Gligic"
+    },
+    {
+      "first_name": "Austin",
+      "last_name": "Eckroat",
+      "full_name": "Austin Eckroat"
+    },
+    {
+      "first_name": "Arjun",
+      "last_name": "Atwal",
+      "full_name": "Arjun Atwal"
+    },
+    {
+      "first_name": "Satoshi",
+      "last_name": "Kodaira",
+      "full_name": "Satoshi Kodaira"
+    },
+    {
+      "first_name": "Doc",
+      "last_name": "Redman",
+      "full_name": "Doc Redman"
+    },
+    {
+      "first_name": "Ryan",
+      "last_name": "Moore",
+      "full_name": "Ryan Moore"
+    },
+    {
+      "first_name": "Matti",
+      "last_name": "Schmid",
+      "full_name": "Matti Schmid"
+    },
+    {
+      "first_name": "Sam",
+      "last_name": "Stevens",
+      "full_name": "Sam Stevens"
+    },
+    {
+      "first_name": "Sean",
+      "last_name": "O'Hair",
+      "full_name": "Sean O'Hair"
+    },
+    {
+      "first_name": "Grayson",
+      "last_name": "Murray",
+      "full_name": "Grayson Murray"
+    },
+    {
+      "first_name": "Jonas",
+      "last_name": "Blixt",
+      "full_name": "Jonas Blixt"
+    },
+    {
+      "first_name": "Paul",
+      "last_name": "Haley II",
+      "full_name": "Paul Haley II"
+    },
+    {
+      "first_name": "Nick",
+      "last_name": "Hardy",
+      "full_name": "Nick Hardy"
+    },
+    {
+      "first_name": "Tyler",
+      "last_name": "Collet",
+      "full_name": "Tyler Collet"
+    },
+    {
+      "first_name": "Austin",
+      "last_name": "Cook",
+      "full_name": "Austin Cook"
+    },
+    {
+      "first_name": "Denny",
+      "last_name": "McCarthy",
+      "full_name": "Denny McCarthy"
+    },
+    {
+      "first_name": "Michael",
+      "last_name": "Kim",
+      "full_name": "Michael Kim"
+    },
+    {
+      "first_name": "Matt",
+      "last_name": "Kuchar",
+      "full_name": "Matt Kuchar"
+    },
+    {
+      "first_name": "Adam",
+      "last_name": "Long",
+      "full_name": "Adam Long"
+    },
+    {
+      "first_name": "Harris",
+      "last_name": "English",
+      "full_name": "Harris English"
+    },
+    {
+      "first_name": "Chris",
+      "last_name": "Stroud",
+      "full_name": "Chris Stroud"
+    },
+    {
+      "first_name": "Curtis",
+      "last_name": "Thompson",
+      "full_name": "Curtis Thompson"
+    },
+    {
+      "first_name": "Henrik",
+      "last_name": "Norlander",
+      "full_name": "Henrik Norlander"
+    },
+    {
+      "first_name": "Brian",
+      "last_name": "Gay",
+      "full_name": "Brian Gay"
+    },
+    {
+      "first_name": "Ryan",
+      "last_name": "Palmer",
+      "full_name": "Ryan Palmer"
+    },
+    {
+      "first_name": "Greg",
+      "last_name": "Chalmers",
+      "full_name": "Greg Chalmers"
+    },
+    {
+      "first_name": "Robert",
+      "last_name": "Streb",
+      "full_name": "Robert Streb"
+    },
+    {
+      "first_name": "Aaron",
+      "last_name": "Rai",
+      "full_name": "Aaron Rai"
+    },
+    {
+      "first_name": "Brian",
+      "last_name": "Stuard",
+      "full_name": "Brian Stuard"
+    },
+    {
+      "first_name": "Andrew",
+      "last_name": "Landry",
+      "full_name": "Andrew Landry"
+    },
+    {
+      "first_name": "Hayden",
+      "last_name": "Buckley",
+      "full_name": "Hayden Buckley"
+    },
+    {
+      "first_name": "Luke",
+      "last_name": "Donald",
+      "full_name": "Luke Donald"
+    },
+    {
+      "first_name": "Callum",
+      "last_name": "Tarren",
+      "full_name": "Callum Tarren"
+    },
+    {
+      "first_name": "Rory",
+      "last_name": "Sabbatini",
+      "full_name": "Rory Sabbatini"
+    },
+    {
+      "first_name": "Max",
+      "last_name": "McGreevy",
+      "full_name": "Max McGreevy"
+    },
+    {
+      "first_name": "Russell",
+      "last_name": "Knox",
+      "full_name": "Russell Knox"
+    },
+    {
+      "first_name": "Zecheng",
+      "last_name": "Dou",
+      "full_name": "Zecheng Dou"
+    },
+    {
+      "first_name": "Parker",
+      "last_name": "Coody",
+      "full_name": "Parker Coody"
+    },
+    {
+      "first_name": "Richy",
+      "last_name": "Werenski",
+      "full_name": "Richy Werenski"
+    },
+    {
+      "first_name": "Marcus",
+      "last_name": "Byrd",
+      "full_name": "Marcus Byrd"
+    },
+    {
+      "first_name": "Nico",
+      "last_name": "Echavarria",
+      "full_name": "Nico Echavarria"
+    },
+    {
+      "first_name": "Camilo",
+      "last_name": "Villegas",
+      "full_name": "Camilo Villegas"
+    },
+    {
+      "first_name": "Kyle",
+      "last_name": "Westmoreland",
+      "full_name": "Kyle Westmoreland"
+    },
+    {
+      "first_name": "Harry",
+      "last_name": "Higgs",
+      "full_name": "Harry Higgs"
+    },
+    {
+      "first_name": "Scott",
+      "last_name": "Brown",
+      "full_name": "Scott Brown"
+    },
+    {
+      "first_name": "Seung-Yul",
+      "last_name": "Noh",
+      "full_name": "Seung-Yul Noh"
+    },
+    {
+      "first_name": "Andrew",
+      "last_name": "Kozan",
+      "full_name": "Andrew Kozan"
+    },
+    {
+      "first_name": "Sung",
+      "last_name": "Kang",
+      "full_name": "Sung Kang"
+    },
+    {
+      "first_name": "Erik",
+      "last_name": "van Rooyen",
+      "full_name": "Erik van Rooyen"
+    },
+    {
+      "first_name": "Thomas",
+      "last_name": "Detry",
+      "full_name": "Thomas Detry"
+    },
+    {
+      "first_name": "Brent",
+      "last_name": "Grant",
+      "full_name": "Brent Grant"
     }
-]
+   ]
