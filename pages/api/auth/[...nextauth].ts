24af6999c28253555238e5f3238e1281fa4ff2d1
Murph Grainger <murphgrainger@Murphs-Macbook.local>
2023-04-06 10:11:26 -0700

    DEBUG: swapped scores scrape auth to getServerSession

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -3,7 +3,6 @@ import GoogleProvider from "next-auth/providers/google";
 import EmailProvider from "next-auth/providers/email";
 import { PrismaAdapter } from "@next-auth/prisma-adapter";
 import { PrismaClient } from "@prisma/client";
-import nodemailer from "nodemailer";
 
 const prisma = new PrismaClient();
 
